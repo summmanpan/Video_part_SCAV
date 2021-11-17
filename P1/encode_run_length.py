dd155736af8226aed8f3fd4f1b2492a1f1c89534
summmanpan <pansuman2011@gmail.com>
2021-11-17 16:21:13 +0100

    p1_fix_some main err

--- P1/encode_run_length.py
+++ P1/encode_run_length.py
@@ -1,5 +1,7 @@
 
 # Encode the message
+# This function is the same as the one in the below
+# but this one is more compact
 def run_length_encode_message2(message):
     """
     :param message: series of bytes as integers within a list
@@ -7,7 +9,6 @@ def run_length_encode_message2(message):
     """
     count = 1;
     final_list = [];
-    #[1,1,1,0,0,0,1]
     for i in range(len(message)-1):
         ele = message[i]
         #check if the element in the list is 0 and is the same with the next
@@ -21,28 +22,6 @@ def run_length_encode_message2(message):
             count = 1;
 
     return final_list
-
-# Encode the message for strings
-def encode_message(message):
-    encoded_string = ""
-    i = 0
-
-    while (i <= len(message) - 1):
-        count = 1
-        ch = message[i]
-        j = i
-        while (j < len(message) - 1):
-            '''if the character at the current index is the same as the character at the next index. If the characters are the same, the count is incremented to 1'''
-            if (message[j] == message[j + 1]):
-                count = count + 1
-                j = j + 1
-            else:
-                break
-        '''the count and the character is concatenated to the encoded string'''
-        encoded_string = encoded_string + str(count) + ch
-        i = j + 1
-
-    return encoded_string
 
 def run_length_encode_message(message):
     """
